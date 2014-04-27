e250cebde0a44853f38acfba627611e14954c0c4
Giulio Cervera <giulio.cervera@cyanogenmod.org>
2014-04-27 17:32:28 +0200

    sirius: bt: remove qcom stuff

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2012 The Android Open Source Project
- * Copyright (C) 2013 The CyanogenMod Project
+ * Copyright (C) 2014 The CyanogenMod Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,13 +20,5 @@
 
 #define BTM_DEF_LOCAL_NAME   "Xperia Z2"
 #define BTA_DISABLE_DELAY 1000 /* in milliseconds */
-
-// Enables Interleave scan
-#define BTA_HOST_INTERLEAVE_SEARCH  TRUE
-// Disables read remote device feature
-#define BTA_SKIP_BLE_READ_REMOTE_FEAT TRUE
-#define MAX_L2CAP_CHANNELS    14
-// skips conn update at conn completion
-#define BTA_BLE_SKIP_CONN_UPD  TRUE
 
 #endif
