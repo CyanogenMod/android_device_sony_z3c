afe6bb480bd61ba47460e60b80d649672d921aed
Giulio Cervera <giulio.cervera@cyanogenmod.org>
2014-09-22 02:43:52 +0200

    sirius: update signature
    Change-Id: I141e000f231dc0103db5da537b49f055f2018992

--- cm.mk
+++ cm.mk
@@ -6,7 +6,7 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
 $(call inherit-product, device/sony/sirius/full_sirius.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=D6503 BUILD_FINGERPRINT=Sony/D6503/D6503:4.4.2/17.1.1.A.0.402/bf53dw:user/release-keys PRIVATE_BUILD_DESC="D6503-user 4.4.2 17.1.1.A.0.402 bf53dw release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=D6503 BUILD_FINGERPRINT=Sony/D6503/D6503:4.4.2/17.1.2.A.0.314/xf5vdw:user/release-keys PRIVATE_BUILD_DESC="D6503-user 4.4.2 17.1.2.A.0.314 xf5vdw release-keys"
 
 PRODUCT_NAME := cm_sirius
 PRODUCT_DEVICE := sirius
