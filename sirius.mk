7662842956806bf10d789fc7cc826d0f305aabaa
Chirayu Desai <chirayudesai1@gmail.com>
2014-11-19 19:40:24 +0530

    Add audio_policy.conf and mixer_paths.xml
    * Moved from vendor
    
    Change-Id: I451bfa7d8ac74a20f43d8cfc32d6d1fdb1c7cb3b

--- sirius.mk
+++ sirius.mk
@@ -43,6 +43,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/recovery/d6502.sh:d6502.sh
 
+# Audio
+PRODUCT_COPY_FILES += \
+   $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
+   $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml
+
 # call dalvik heap config
 $(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-dalvik-heap.mk)
 
