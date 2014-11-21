fc2f306a7f87cb8e80a616fd4e1d244a1a0bc74b
Chirayu Desai <chirayudesai1@gmail.com>
2014-11-21 20:04:22 +0530

    Open source thermal management
    * Use Sony's open source thermanager
    * Get rid of their proprietary implementation, sysmon
    
    Change-Id: I7e41d6b18273f669358b50616de57be212242151

--- sirius.mk
+++ sirius.mk
@@ -48,6 +48,10 @@ PRODUCT_COPY_FILES += \
    $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
    $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml
 
+# Thermal manager
+PRODUCT_COPY_FILES += \
+   $(LOCAL_PATH)/thermanager.xml:system/etc/thermanager.xml
+
 # call dalvik heap config
 $(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-dalvik-heap.mk)
 
