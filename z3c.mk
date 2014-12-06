cc24aef7908cce827cbd4eb878331e82a2cc0e7e
Giulio Cervera <giulio.cervera@cyanogenmod.org>
2014-12-06 14:35:14 +0100

    update usb product id

--- z3c.mk
+++ z3c.mk
@@ -42,12 +42,12 @@ PRODUCT_COPY_FILES += \
 
 # Audio
 PRODUCT_COPY_FILES += \
-   $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
-   $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml
+    $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
+    $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml
 
 # Thermal manager
 PRODUCT_COPY_FILES += \
-   $(LOCAL_PATH)/thermanager.xml:system/etc/thermanager.xml
+    $(LOCAL_PATH)/thermanager.xml:system/etc/thermanager.xml
 
 # call dalvik heap config
 $(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
