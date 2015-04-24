4daf09dedb3f9c7d6b7ed520d1cad654cdb20bd5
Giulio Cervera <giulio.cervera@cyanogenmod.org>
2015-04-24 18:31:07 +0200

    update signature
    Change-Id: I7ecea341d83b8c7aa22fe5d817220115b2c16f67

--- cm.mk
+++ cm.mk
@@ -7,8 +7,8 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 $(call inherit-product, device/sony/z3c/full_z3c.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=D5803
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=Sony/D5803/D5803:4.4.4/23.0.A.2.105/lvf_Rw:user/release-keys
-PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="D5803-user 4.4.4 23.0.A.2.105 lvf_Rw release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=Sony/D5803/D5803:5.0.2/23.1.A.0.690/814068233:user/release-keys
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="D5803-user 5.0.2 23.1.A.0.690 814068233 release-keys"
 
 PRODUCT_NAME := cm_z3c
 PRODUCT_DEVICE := z3c
